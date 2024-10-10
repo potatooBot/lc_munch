3905cbb68304e9a1c4870121c3f680b1dfdc8057
Keshav <81036483+potatooBot@users.noreply.github.com>
2024-10-10 13:00:09 +0530

    Time: 39 ms (11.94%), Space: 55 MB (29.6%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.java
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -9,15 +9,14 @@ class Solution {
 
         Arrays.sort(arr,(a,b)->Integer.compare(a[1],b[1]));
 
-      //  for(int rows[]:arr)
-//System.out.println(Arrays.toString(rows));
+
 
         int ans=0;
         int mini=arr[0][0];
         for(int i=0;i<arr.length;i++){
             ans=Math.max(ans,arr[i][0]-mini);
             mini=Math.min(mini,arr[i][0]);
-           // System.out.println(mini);
+
         }
         return ans;
     }
